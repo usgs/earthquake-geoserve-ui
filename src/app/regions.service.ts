386b088cdedcaaf93687a3919f7bf42d7f69f452
jdbrown-usgs <jdbrown@usgs.gov>
2017-12-04 15:26:27 -0700

    created region service

--- src/app/regions.service.ts
+++ src/app/regions.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+
 
 @Injectable()
 export class RegionsService {
+  private baseUrl = 'https://earthquake.usgs.gov/ws/geoserve/regions.json';
+  private regions = new BehaviorSubject<any>({});
 
-  constructor() { }
+  currentRegions = this.regions.asObservable();
 
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getRegions (latitude:string, longitude:string) {
+    let url
+
+    url = `${this.baseUrl}?latitude=${latitude}&longitude=${longitude}`;
+
+    this.http.get<any>(url).subscribe((data) => {
+      this.regions.next(data);
+    });
+  }
 }
