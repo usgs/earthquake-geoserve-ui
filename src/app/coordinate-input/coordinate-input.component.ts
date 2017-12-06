628eab3bdfa886a8e6ca12924ad36b88697eef52
Eric Martinez <emartinez@usgs.gov>
2017-12-06 15:59:17 -0700

    Updated coordinate control to use places service.

--- src/app/coordinate-input/coordinate-input.component.ts
+++ src/app/coordinate-input/coordinate-input.component.ts
@@ -1,23 +1,23 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
+
+import { PlacesService } from '../places.service';
+
+
 @Component({
   selector: 'app-coordinate-input',
   templateUrl: './coordinate-input.component.html',
   styleUrls: ['./coordinate-input.component.css']
 })
 export class CoordinateInputComponent implements OnInit {
-  @Output() search: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor() { }
+  constructor (private placesService: PlacesService) { }
 
-  ngOnInit() {
+  ngOnInit () {
   }
 
   handleClick (latitude: string, longitude: string) {
-    this.search.emit({
-      latitude: +latitude,
-      longitude: +longitude
-    });
+    this.placesService.getPlaces(latitude, longitude);
   }
 
 }
