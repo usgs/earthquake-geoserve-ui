571ed8c4507a4b02c545fd76b224899598de2381
jdbrown-usgs <jdbrown@usgs.gov>
2017-12-14 11:07:39 -0700

    removed unneeded code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // TODO, set other region variables
-  admin: Region[];
-
   TITLE = 'Geoserve';
 
   NAVIGATION = [
