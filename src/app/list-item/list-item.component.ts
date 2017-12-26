26b6664d953056aa67c2292486b878a39cc4f2b8
jdbrown-usgs <jdbrown@usgs.gov>
2017-12-26 08:58:56 -0700

    added inputs

--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list-item',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListItemComponent implements OnInit {
 
+  @Input() TITLE: any;
+  @Input() DATA: any;
   constructor() { }
 
   ngOnInit() {
