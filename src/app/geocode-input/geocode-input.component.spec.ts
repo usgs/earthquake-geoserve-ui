6994c6e0031bcd6a56b3affa945bd8c359f8c9c8
ehunter-usgs <ehunter@usgs.gov>
2018-01-11 09:51:21 -0700

    Fix broken reference to old Coordinates object

--- src/app/geocode-input/geocode-input.component.spec.ts
+++ src/app/geocode-input/geocode-input.component.spec.ts
@@ -26,7 +26,7 @@ describe('GeocodeInputComponent', () => {
     latitude: 39.756650000000036,
     longitude: -105.22494999999998,
     method: 'geocode',
-    name: 'Golden, Colorado',
+    place: 'Golden, Colorado',
     zoom: 9
   };
 
