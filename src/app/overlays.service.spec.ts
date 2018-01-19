7cd9c28806e0361a053344e9e4f945eac11655fd
ehunter-usgs <ehunter@usgs.gov>
2018-01-19 15:49:25 -0700

    fix broken tests

--- src/app/overlays.service.spec.ts
+++ src/app/overlays.service.spec.ts
@@ -1,12 +1,25 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, getTestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { OverlaysService } from './overlays.service';
 
 describe('OverlaysService', () => {
+  let httpClient: HttpTestingController,
+      injector: TestBed;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
       providers: [OverlaysService]
     });
+    injector = getTestBed();
+    httpClient = injector.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpClient.verify();
   });
 
   it('should be created', inject([OverlaysService], (service: OverlaysService) => {
