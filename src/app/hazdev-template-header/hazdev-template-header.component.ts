31075c657d72e54d41ab5c481f9c737e6af95200
ehunter-usgs <ehunter@usgs.gov>
2018-02-06 17:32:59 -0700

    Setup basic theming with a blue-grey palette

--- src/app/hazdev-template-header/hazdev-template-header.component.ts
+++ src/app/hazdev-template-header/hazdev-template-header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hazdev-template-header',
   templateUrl: './hazdev-template-header.component.html',
-  styleUrls: ['./hazdev-template-header.component.css']
+  styleUrls: ['./hazdev-template-header.component.scss']
 })
 export class HazdevTemplateHeaderComponent implements OnInit {
 
